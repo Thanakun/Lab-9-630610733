3902b4597281c76de0922d2abab503702562e640
Thanakun <Thanakun_Ch@cmu.ac.th>
2021-01-08 16:36:56 +0700

    <3

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,37 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int i = 0;
+    double sum = 0.0;
+    double sum1 = 0.0;
+    long double mean = 0.0;
+    long double standard = 0.0;
+    string textline;
+   
+    ifstream score("score.txt");
+    while(getline(score,textline)){
+
+
+        sum += atof(textline.c_str());
+        sum1 = sum1 + pow(atof(textline.c_str()),2);
+        i++;
+    }
+
+    
+
+
+    mean = (sum/i);
+    standard = sqrt((sum1/i)-pow(mean,2));
+
+
+    cout << "Number of data = " << i << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << standard << endl;
+}
